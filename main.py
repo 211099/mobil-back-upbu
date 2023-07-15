f4b19df348c3f450e609221228a292d02df613af
211099 <211099@ids.upchiapas.edu.mx>
2023-07-14 21:43:57 -0600

    cambios server port

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from app import app
 from utils.db import db
 from waitress import serve
 
+import os
+
+
 with app.app_context():
     db.init_app(app)
     db.create_all()
     print('creado')
 
 if __name__ == "__main__":
-     serve(app, host='0.0.0.0', port=5000)
+     serve(app, host='0.0.0.0', port=os.getenv("PORT", default=5000))
  
